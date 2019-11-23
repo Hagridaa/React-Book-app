6baf423abbdc24d3b0795fddc9263c9ef7415be5
hagridaa <annekristiinarasanen@gmail.com>
2019-11-23 11:52:40 +0200

    Poistettu ylim. import

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,7 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { Link } from "react-router-dom";
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
